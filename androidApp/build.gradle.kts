ede3f66a0e86760b6897b98343a703cfffe2b492
adrcotfas <adrcotfas@gmail.com>
2025-03-13 22:53:06 +0200

    chore: update version code

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = packageName
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 300
+        versionCode = 304
         versionName = "3.0.0"
     }
     buildFeatures {
