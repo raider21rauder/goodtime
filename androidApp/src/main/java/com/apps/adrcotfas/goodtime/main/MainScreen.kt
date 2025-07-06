c767f4f67a5e7a9dc37f43d1449e921a37762042
adrcotfas <adrcotfas@gmail.com>
2025-07-06 10:24:08 +0300

    fix(MainScreen): padding

--- androidApp/src/main/java/com/apps/adrcotfas/goodtime/main/MainScreen.kt
+++ androidApp/src/main/java/com/apps/adrcotfas/goodtime/main/MainScreen.kt
@@ -42,7 +42,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
@@ -303,7 +302,6 @@ fun MainScreen(
                     )
                     if (showTutorial) {
                         TutorialScreen(
-                            modifier = Modifier.padding(padding),
                             onClose = { viewModel.setShowTutorial(false) },
                         )
                     } else {
